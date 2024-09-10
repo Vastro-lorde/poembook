39c5490516e234d973a8de905e5f9382be869ad3
Vastro-lorde <omatsolaseund@gmail.com>
2024-09-10 10:11:12 +0100

    Add project files.

---
+++ poembook/Controllers/PoemController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace poembook.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PoemController : ControllerBase
+    {
+    }
+}
---
+++ poembook/NewFolder/PoemModel.cs
@@ -0,0 +1,13 @@
+﻿namespace poembook.NewFolder
+{
+    public class PoemModel
+    {
+        public required string Title { get; set; }
+
+        public required string Content { get; set; }
+
+        public string? Author { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
